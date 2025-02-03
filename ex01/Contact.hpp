358a04c724751e967c135c8b6785fedbfc286be5
Erya Wu <ewu@2-G-2.42heilbronn.de>
2025-02-03 14:22:06 +0100

    hpp setting

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,9 +6,15 @@
 /*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 13:54:23 by ewu               #+#    #+#             */
-/*   Updated: 2025/02/03 13:57:46 by ewu              ###   ########.fr       */
+/*   Updated: 2025/02/03 14:18:37 by ewu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+# include <iostream>
+# include <string>
 
 /**
 anything that will always be used inside a class is private
@@ -16,5 +22,15 @@ first name, last name, nickname, number, drakest screct
 */
 class Contact
 {
-	
-}
+private: /*variables should not be acceesed*/
+	std::string secret;
+	std::string first_n;
+	std::string last_n;
+	std::string nick_n;
+	int phone_nbr;	
+public: /*define method(function) inside Contact class*/
+	void addContact();
+	void searchC(); /*functions will be declared outside class*/
+};
+
+#endif
