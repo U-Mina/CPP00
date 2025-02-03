1f5d59cf9c9c6f61f91924bbc1f0bbdf18684760
Erya Wu <ewu@2-G-2.42heilbronn.de>
2025-02-03 12:25:42 +0100

    finishe ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,38 @@
 /*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 11:18:08 by ewu               #+#    #+#             */
-/*   Updated: 2025/02/03 11:18:38 by ewu              ###   ########.fr       */
+/*   Updated: 2025/02/03 12:23:47 by ewu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cctype>
+#include <string>
+
+int main(int ac, char **av)
+{
+	int i;
+	int j;
+	
+	i = 1;
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std ::endl;
+		return 0;
+	}
+	else
+	{
+		while (i < ac)
+		{
+			j = 0;
+			while (av[i][j])
+			{
+				std::cout << (char)std::toupper(av[i][j]);
+				j++;
+			}
+			i++;
+		}
+	}
+	std::cout << std::endl;
+	return 0;
+}
