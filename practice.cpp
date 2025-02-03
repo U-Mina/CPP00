8709ea3261b19d4f006b9fbdb0868937263fc1f9
Erya Wu <ewu@2-G-2.42heilbronn.de>
2025-02-03 11:32:02 +0100

    std cin and std cout with endl or \n practice

--- practice.cpp
+++ practice.cpp
@@ -6,10 +6,20 @@
 /*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 11:19:01 by ewu               #+#    #+#             */
-/*   Updated: 2025/02/03 11:19:29 by ewu              ###   ########.fr       */
+/*   Updated: 2025/02/03 11:30:49 by ewu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 using namespace std;
 
+string input;
+
+int main()
+{
+	cout << "this is wiith namespace std::cout\n";
+	cout << "input some sentences\n";
+	getline(cin, input);
+	cout << "is this what you typed:" << input << endl;
+	return 0;
+}
