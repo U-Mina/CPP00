bb280802e9d63e4e1eff8a8935ce9e1565234e40
Erya Wu <ewu@2-G-2.42heilbronn.de>
2025-02-03 11:50:03 +0100

    correct way to do null check of string in cpp

--- practice.cpp
+++ practice.cpp
@@ -6,20 +6,30 @@
 /*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 11:19:01 by ewu               #+#    #+#             */
-/*   Updated: 2025/02/03 11:30:49 by ewu              ###   ########.fr       */
+/*   Updated: 2025/02/03 11:49:04 by ewu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 using namespace std;
-
-string input;
 
 int main()
 {
-	cout << "this is wiith namespace std::cout\n";
-	cout << "input some sentences\n";
+	string input;
+	string output;
+	int i;
+	cout << "what is your input?" << endl;
 	getline(cin, input);
-	cout << "is this what you typed:" << input << endl;
+	if (input == "null")
+	{
+		cout << "* LOUD AND UNVEARABLE FEEDBACK NOISE *" << "\n";
+	}
+	else
+	{
+		for (i = 0; input[i]; i++)
+			output[i] += toupper(input[i]);
+		cout << output << endl;
+	}
 	return 0;
 }
